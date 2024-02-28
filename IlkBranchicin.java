e6c933e07e19406142fccdc06562fd5e0db38bbf
AyseSEF <161112594+AyseSEF@users.noreply.github.com>
2024-02-28 11:22:24 +0100

    Update IlkBranchicin.java

--- IlkBranchicin.java
+++ IlkBranchicin.java
@@ -1,5 +1,6 @@
 public class IlkBranchicin {
 
     //bu benim ilk comitim olucak
-// Buda benim ikinci komutum olucak hemide terminal ile
+    // Buda benim ikinci komutum olucak hemide terminal ile
+    // main branchete biseyler degisti hemide github uzerinde localden degil
 }
